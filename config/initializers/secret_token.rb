bb9a42b14d5d064019fb5bea6030b0893da98df1
Wonsup Lee <onesup.lee@gmail.com>
2012-11-12 01:29:34 +0900

    새 프로젝트
    생각해보니 한 프로젝트 ios 안드로이드 웹을 같이 담아놓을 필요가 없어서. 어차피 지금 공개프로젝트로 진행하는거니까. 그래서
    이 프로젝트를 웹 전용으로 돌림. 카피스트라노+유니콘을 이용한 배포를 하려고 하는데 예제 그대로 실행을 못하고 (수많은 삽질이
    예상되는) 응용이 필요한 부분이 있기도 하고.
    https://medrails.wordpress.com/2012/04/10/ubuntu-nginx-unicorn-capistran
    o/

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P2pBookSharing::Application.config.secret_token = '479d412f8e5cb648ee798928fc7bcb9fa12f36a3ba13814cc8fd702d055a2b0e70930c26795f657cc2914640c77fd286bfe9dfb8adec996a01cbe36c7536b56e'
