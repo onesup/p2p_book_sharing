4f63c3e209cbcacdf3f86146ee31c61f53522dcd
Wonsup Lee <onesup.lee@gmail.com>
2012-11-13 11:57:22 +0900

    application directory change

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P2pBookSharing::Application.config.secret_token = 'ec115f1b325f1b77f86a66171627de6a9c39cd5cc10033c6cdf9c3ea0c78a813e3becf5bb1157a4b5ea3f58735fd584882c5d5d29b41da428427b115462195bf'
