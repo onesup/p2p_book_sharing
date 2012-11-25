6e732808df667ea1e42f5788c3b23139a81f5d00
Wonsup Lee <onesup.lee@gmail.com>
2012-11-25 17:24:41 +0900

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
 root = "/home/moncl/p2p_book_sharing/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+stderr_path "#{root}/log/unicorn_stderr.log"
+stdout_path "#{root}/log/unicorn_stdout.log"
 
 listen "/tmp/unicorn.p2p_book_sharing.sock"
 worker_processes 2
@@ -11,3 +11,4 @@ timeout 30
 before_exec do |server| 
   ENV["BUNDLE_GEMFILE"] = "/p2p_book_sharing/current/Gemfile" 
 end
+
